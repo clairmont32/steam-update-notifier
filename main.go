3b251705fe5e40efc05cde540e9b08e0aad4a1c4
Matthew Clairmont <matthew.clairmont1@gmail.com>
2019-10-20 12:16:01 -0400

    formalize message string for news

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"strconv"
-
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -14,6 +12,15 @@ import (
 	"os"
 	"time"
 )
+
+func getWebhookURL() string {
+	if len(os.Getenv("discord")) != 0 {
+		return os.Getenv("discord")
+	} else {
+		log.Fatalf("No discord webhook in environment variable!")
+	}
+	return ""
+}
 
 func readConfig() {
 	configFile, openErr := os.Open("config.yaml")
@@ -27,6 +34,10 @@ func readConfig() {
 	for scanner.Scan() {
 		configContents = append(configContents, scanner.Text())
 	}
+}
+
+func getGameName() {
+	// TODO reach out to steam API to lookup game title
 }
 
 func getNewsContent(url string) bytes.Buffer {
@@ -54,11 +65,13 @@ func getNewsContent(url string) bytes.Buffer {
 		fmt.Println("Received a HTTP 429 response. Sleeping for 10s!")
 		time.Sleep(10 * time.Second)
 		getNewsContent(url)
+
 	} else if resp.StatusCode != http.StatusOK {
 		log.Fatalf("Received an error from %v. Exiting...\nError: %v\nBody: %v\n", url, resp.Status, resp.Body)
 
 	} else {
 		numRead, bufErr := buffer.ReadFrom(resp.Body)
+		_ = resp.Body.Close()
 		if bufErr != nil && numRead < 1 {
 			log.Fatalf("Could not load response body into content variable. Error: %v", bufErr)
 		}
@@ -68,7 +81,7 @@ func getNewsContent(url string) bytes.Buffer {
 	return bytes.Buffer{}
 }
 
-type SteamResponse struct {
+type NewsResponse struct {
 	AppNews struct {
 		AppID     int `json:"appid"`
 		NewsItems []struct {
@@ -85,30 +98,33 @@ type DiscordText struct {
 }
 
 func postToDiscord(content string) {
-	webhookURL := ""
+	// set webhook URL from env vars
+	webhookURL := getWebhookURL()
 
+	// parse message into json struct for the payload
 	payload := DiscordText{Content: content}
 	jsonContent, marshErr := json.Marshal(&payload)
 	if marshErr != nil {
 		log.Fatalf("Could not marshal message. Error: %v", marshErr)
 	}
+
+	// form HTTP request
 	req, reqErr := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonContent))
 	if reqErr != nil {
 		log.Fatalf("Could not make HTTP Request for Discord. Error: %v", reqErr)
 	}
-
-	// create payload and add it to the header
 	req.Header.Add("content-type", "application/json")
 
-	// http client
+	// create HTTP client and do the request
 	client := http.Client{Timeout: 5 * time.Second}
 	response, respErr := client.Do(req)
 	if respErr != nil {
 		log.Fatalf("Could not form HTTP client. Error: %v", respErr)
 	}
 
+	// handle status codes that we dont care about
 	if response.StatusCode != http.StatusNoContent {
-		log.Printf("HTTP POST failed. Status Code: %v", response.StatusCode)
+		log.Printf("HTTP POST to Discord server failed. Status Code: %v", response.StatusCode)
 		body, readErr := ioutil.ReadAll(response.Body)
 		if readErr != nil {
 			log.Fatalf("Could not read response body. Error: %v", readErr)
@@ -117,22 +133,26 @@ func postToDiscord(content string) {
 	} else {
 		fmt.Println("Success!")
 	}
+}
 
+func formatNewsMessage(content NewsResponse, appID int) string {
+	var messageString string
+	for _, item := range content.AppNews.NewsItems {
+		messageString = fmt.Sprintf("New news post detected for %v\n%v\n%v", appID, item.Title, item.Url)
+	}
+	return messageString
 }
 
 func main() {
-	url := "https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=717790&count=1"
-	data := getNewsContent(url)
-	var steamResponse SteamResponse
-	jsonErr := json.Unmarshal(data.Bytes(), &steamResponse)
-	if jsonErr != nil {
-		log.Fatalf("Could not process API response. Error: %v", jsonErr)
+	appIDs := []int{717790, 107410, 552990}
+	for _, appID := range appIDs {
+		url := fmt.Sprintf("https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=%v&count=1", appID)
+		data := getNewsContent(url)
+		var steamResponse NewsResponse
+		jsonErr := json.Unmarshal(data.Bytes(), &steamResponse)
+		if jsonErr != nil {
+			log.Fatalf("Could not process API response. Error: %v", jsonErr)
+		}
+		postToDiscord(formatNewsMessage(steamResponse, appID))
 	}
-	for _, item := range steamResponse.AppNews.NewsItems {
-		postToDiscord("New update detected for Hold Your Own!")
-		postToDiscord(item.Title)
-		postToDiscord(fmt.Sprintf("Date posted: %v", strconv.Itoa(item.Date)))
-		postToDiscord(item.Url)
-	}
-
 }
